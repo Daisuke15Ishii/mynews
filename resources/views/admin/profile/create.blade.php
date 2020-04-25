900fd192a9d42d99ed47bf9ca9e25b9d545fef8c
”名前” <>
2020-04-25 15:34:07 +0000

    初コミット(環境構築)

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MyNews</title>
+    </head>
+    <body>
+        <h1>MyProfile作成画面(create)</h1>
+        <p>create画面です</p>
+    </body>
+</html>

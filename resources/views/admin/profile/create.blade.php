4d7edbbc2ce43720ca240a5e2a8c5cde3fe6ab4c
”名前” <>
2020-04-26 04:37:46 +0000

    third commit(kadai laravel 11_update1)

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,28 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
 
-        <title>MyNews</title>
-    </head>
-    <body>
-        {{-- layouts/profile.blade.phpを読み込む --}}
-        @extends('layouts.profile')
-        
-        {{-- profile.blade.phpの@yield('title')に'Myプロフィール'を埋め込む --}}
-        @section('title', 'Myプロフィール')
-        
-        {{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
-        @section('content')
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 mx-auto">
-                        <h2>Myプロフィール(create)</h2>
-                    </div>
-                </div>
+{{-- profile.blade.phpの@yield('title')に'Myプロフィール'を埋め込む --}}
+@section('title', 'Myプロフィール')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール(create)</h2>
             </div>
-        @endsection
-    </body>
-</html>
+        </div>
+    </div>
+@endsection

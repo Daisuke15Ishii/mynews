49f3518f6ac4767655647585e240aa040b0dc3cb
”名前” <>
2020-04-29 11:35:31 +0000

    laravel kadai14_NewsToukou

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+// 以下を追記することでProfiles Modelが扱えるようになる
+use App\Profiles;
+
 class ProfileController extends Controller
 {
     //以下、追記分
@@ -12,7 +15,20 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
 
-    public function create(){
+    public function create(Request $request){
+      // Varidationを行う
+      $this->validate($request, Profiles::$rules);
+
+      $profiles = new Profiles;
+      $form = $request->all();
+
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+
+      // データベースに保存する
+      $profiles->fill($form);
+      $profiles->save();
+
         return redirect('admin/profile/create');
     }
 
